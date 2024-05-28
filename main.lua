6a2eb7b7f06660cff03c025263294be14b82fbd3
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-05-28 03:14:48 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
--- Toml-helper v1.0.0
+-- Toml-helper v1.0.1
 -- SmoothSpatula
 
 log.info("Successfully loaded ".._ENV["!guid"]..".")
@@ -8,26 +8,26 @@ log.info("Successfully loaded ".._ENV["!guid"]..".")
 tomlfuncs = true
 
 local self = {}
-local cfg_path = "ReturnOfModding/config"
+local cfg_path = path.get_parent(_ENV["!config_mod_folder_path"])
 local cfg_name = "cfg.toml"
-local full_cfg_path = path.get_parent(path.get_parent(_ENV["!plugins_mod_folder_path"]))
+
 
 -- ========== Functions ==========
 
-load_cfg = function(plugin_path)
-    local full_path = path.combine(cfg_path, plugin_path, cfg_name)
+load_cfg = function(plugin_name)
+    local full_path = path.combine(cfg_path, plugin_name, cfg_name)
     local succeeded, loaded_table = pcall(toml.decodeFromFile, full_path)
     if not succeeded then
         print("Error loading "..full_path)
         return nil
     end
-    log.info("config file successfully loaded")
+    log.info("Config file for "..plugin_name.." successfully loaded")
     return loaded_table
 end
 
-save_cfg = function (plugin_path, table)
-    verify_folder(plugin_path)
-    local full_path = path.combine(cfg_path, plugin_path, cfg_name)
+save_cfg = function (plugin_name, table)
+    if verify_folder(plugin_name) == nil then return nil end
+    local full_path = path.combine(cfg_path, plugin_name, cfg_name)
     succeeded, documentOrErrorMessage = pcall(toml.encodeToFile, table, { file = full_path, overwrite = true })
     if not succeeded then
         print(documentOrErrorMessage)
@@ -37,29 +37,40 @@ save_cfg = function (plugin_path, table)
 end
 
 -- Makes sure the config folder exists (Toml can't create it)
-verify_folder = function(plugin_path)
-    local create_dir_path = path.combine(full_cfg_path, "config", plugin_path)
-    if gm.directory_exists(create_dir_path) ==.0 then
-        log.info("Creating config directory")
-        gm.directory_create(create_dir_path)
+verify_folder = function(plugin_name)
+
+    local full_cfg_path = path.combine(cfg_path, plugin_name)
+    local dirs = path.get_directories(cfg_path)
+    for _,v in pairs(dirs) do
+        if v == full_cfg_path then return 0 end
     end
+
+    directoryCreated = path.create_directory(full_cfg_path)
+    if not directoryCreated then
+        print("Couldn't create config folder for " .. plugin_name)
+        return nil
+    end
+    return 0
 end
 
 -- Call this
-config_update = function(plugin_path, default_table)
-    local loaded_table = load_cfg(plugin_path)
+config_update = function(plugin_name, default_table)
+    local loaded_table = load_cfg(plugin_name)
     -- If config doesn't exist, create it
     if not loaded_table then
-        save_cfg(plugin_path, default_table)
+        save_cfg(plugin_name, default_table)
         return default_table
     end
     -- If it does exist, fill in missing fields  (should we also clean up extra fields?)
     for k, v in pairs(default_table) do
         if loaded_table[k] == nil then
-            print("Adding "..k.." field to "..plugin_path.." config")
+            print("Adding "..k.." field to "..plugin_name.." config")
             loaded_table[k] = v
         end
     end
+    save_cfg(plugin_name, loaded_table)
+    return loaded_table
+end
     save_cfg(plugin_path, loaded_table)
     return loaded_table
 end
